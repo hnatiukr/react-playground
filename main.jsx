eb0494c36102bef34a7a7aa89125cd930228d766
Roman Hnatiuk <hnatiukr@pm.me>
2025-01-03 20:37:17 +0100

    Init template with `react@18` by default

---
+++ main.jsx
@@ -0,0 +1,9 @@
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+);
+
+function Main() {
+  return <div>Hello, World!</div>;
+}
